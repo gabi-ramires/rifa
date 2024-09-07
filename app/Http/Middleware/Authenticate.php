03b74c637ce784a2a982b8460a9e59ce2932bdc0
Gabriela Ramires <engenheira.ramires@gmail.com>
2024-09-07 16:11:44 -0300

    autenticando as rotas

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,35 +1,19 @@
 <?php
+// app/Http/Middleware/Authenticate.php
 namespace App\Http\Middleware;
 
-use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-class Authenticate extends Middleware
+class Authenticate
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     */
-    protected function redirectTo(Request $request): ?string
+    public function handle(Request $request, Closure $next, ...$guards)
     {
-
-        // Para APIs, retorne uma resposta JSON com o código 401
-        return response()->json(['message' => 'Unauthenticated.'], 401);
-        
-    }
-
-    /**
-     * Handle an unauthenticated user.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  string[]  $guards
-     * @return \Illuminate\Http\Response
-     */
-    protected function unauthenticated($request, array $guards)
-    {
-        if ($request->expectsJson()) {
-            return response()->json(['message' => 'Unauthenticated.'], 401);
+        if (!Auth::guard('sanctum')->check()) {
+            return response()->json(['message' => 'Não autenticado.'], 401);
         }
 
-        return response()->json(['message' => 'Unauthenticated.'], 401);
+        return $next($request);
     }
 }
